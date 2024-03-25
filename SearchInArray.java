281057454c59b9d33aab9a358484b6473a0d9552
brijithalal <97222631+brijithalal@users.noreply.github.com>
2024-03-25 23:16:24 +0530

    Linear Search

---
+++ SearchInArray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class SearchInArray {
+    public static void main(String[] args) {
+        int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+        int target = 19;
+        int ans = linearSearch(nums,  target);
+        System.out.println(ans);
+
+    }
+    // search in the array: return the index if item found
+    // otherwise if item ot found return -1
+
+    static int linearSearch(int[] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+        // run a for loop
+        for (int index = 0; index < arr.length; index++) {
+            //check for element at every index if it is a target
+            int element = arr[index];
+            if(element == target) {
+                return index;
+            }
+            
+        }
+        return -1;
+    }
+}
